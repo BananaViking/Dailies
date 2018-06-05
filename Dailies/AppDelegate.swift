93822041593a84894026b56515dea90ae58014df
Banana Viking <bananavikingcoder@gmail.com>
2018-06-05 11:30:28 -0500

    fix app delegate mess and get first launch of day working

--- Dailies/AppDelegate.swift
+++ Dailies/AppDelegate.swift
@@ -11,85 +11,72 @@ import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         let center = UNUserNotificationCenter.current()
         center.delegate = self
         
-//        UserDefaults.standard.set(Date(), forKey: "lastLaunch")
-
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
         
-        let lastLaunch = UserDefaults.standard.object(forKey: "lastLaunch") as? Date
-        let currentLaunch = Date().timeIntervalSince(lastLaunch!)
+        let lastLaunchDate = dateFormatter.string(from: UserDefaults.standard.object(forKey: "lastLaunch") as! Date)
         
-        print("last launch: \(UserDefaults.standard.object(forKey: "lastLaunch") as? Date)")
-        print("current launch: \(currentLaunch)")
+        let today = Date()
+        let todayDate = dateFormatter.string(from: today)
         
-//        let currentDate = Date()
-//        let formatter = DateFormatter()
-//        formatter.dateStyle = .short
-//        print(formatter.string(from: currentDate))
-//        print("currentDate is \(currentDate)")
+        print("lastLaunch: \(UserDefaults.standard.object(forKey: "lastLaunch") as! Date)")
+        print("today: \(today)")
+        print("lastLaunchDate: \(lastLaunchDate)")
+        print("todayDate: \(todayDate)")
         
-        // get the time the app last launched (returns 0.0 if key doesn't exist)
-//        let lastLaunch = UserDefaults.standard.double(forKey: "lastLaunch")
-//        let lastLaunchDate = NSDate(timeIntervalSince1970: lastLaunch)
-        
-        //check to see if lastLaunchDate is today
-//        let lastLaunchIsToday = NSCalendar.current.isDateInToday(lastLaunchDate as Date)
-//
-//        if !lastLaunchIsToday {
-//
-//        }
-        
-//        print("lastLaunch = \(lastLaunch)")
-//        print("lastLaunchDate = \(lastLaunchDate)")
-//        print("lastLaunchIsToday = \(lastLaunchIsToday)")
-        
-        // update the last launch value
-//        UserDefaults.set(Double(NSDate().timeIntervalSince1970()), forKey: "lastLaunch")
-//        UserDefaults.set(NSDate().timeIntervalSince1970(), forKey: "lastLaunch")
+        if lastLaunchDate == todayDate {
+            print("You have already logged in today.")
+        } else {
+            print("This is your first login of the day.")
+        }
         
         
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
         UserDefaults.standard.set(Date(), forKey: "lastLaunch")
-        print(UserDefaults.standard.object(forKey: "lastLaunch") as? Date)
-
+        print(UserDefaults.standard.object(forKey: "lastLaunch")!)
+        
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
         UserDefaults.standard.set(Date(), forKey: "lastLaunch")
-        print(UserDefaults.standard.object(forKey: "lastLaunch") as? Date)
-
+        print(UserDefaults.standard.object(forKey: "lastLaunch")!)
     }
     
     // MARK: - User Notification Delegates
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         print("Received local notification \(notification)")
     }
-
-
+    
+    
 }
 
