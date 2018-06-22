3755530b952a3e12c959376a3e748b823fc01821
Banana Viking <bananavikingcoder@gmail.com>
2018-06-22 15:34:09 -0500

    change enemyImage to enemyImageView

--- Dailies/View Controllers/LandscapeViewController.swift
+++ Dailies/View Controllers/LandscapeViewController.swift
@@ -15,28 +15,28 @@ class LandscapeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let enemyImage  = self.view.viewWithTag(9000) as! UIImageView
+        let enemyImageView = self.view.viewWithTag(9000) as! UIImageView
         
         if playerLevel == 1 {
-            enemyImage.image = UIImage(named: "enemy1")
+            enemyImageView.image = UIImage(named: "enemy1")
         } else if playerLevel == 2 {
-            enemyImage.image = UIImage(named: "enemy2")
+            enemyImageView.image = UIImage(named: "enemy2")
         } else if playerLevel == 3 {
-            enemyImage.image = UIImage(named: "enemy3")
+            enemyImageView.image = UIImage(named: "enemy3")
         } else if playerLevel == 4 {
-            enemyImage.image = UIImage(named: "enemy4")
+            enemyImageView.image = UIImage(named: "enemy4")
         } else if playerLevel == 5 {
-            enemyImage.image = UIImage(named: "enemy5")
+            enemyImageView.image = UIImage(named: "enemy5")
         } else if playerLevel == 6 {
-            enemyImage.image = UIImage(named: "enemy6")
+            enemyImageView.image = UIImage(named: "enemy6")
         } else if playerLevel == 7 {
-            enemyImage.image = UIImage(named: "enemy7")
+            enemyImageView.image = UIImage(named: "enemy7")
         } else if playerLevel == 8 {
-            enemyImage.image = UIImage(named: "enemy8")
+            enemyImageView.image = UIImage(named: "enemy8")
         } else if playerLevel == 9 {
-            enemyImage.image = UIImage(named: "enemy9")
+            enemyImageView.image = UIImage(named: "enemy9")
         } else if playerLevel >= 10 {
-            enemyImage.image = UIImage(named: "enemy10")
+            enemyImageView.image = UIImage(named: "enemy10")
         }
     }
 }
