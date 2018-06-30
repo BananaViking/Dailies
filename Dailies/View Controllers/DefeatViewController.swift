006af6703a7a8b8656049c803177be71657ac0b4
Banana Viking <bananavikingcoder@gmail.com>
2018-06-30 15:05:48 -0500

    hooked up button for dismissDefeatVC

--- Dailies/View Controllers/DefeatViewController.swift
+++ Dailies/View Controllers/DefeatViewController.swift
@@ -10,5 +10,13 @@ import UIKit
 
 class DefeatViewController: UIViewController {
     
-    
+    let vc = DailiesViewController()
+
+    @IBAction func dismissDefeatVC(_ sender: UIButton) {
+        self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
+        vc.playSound(forObject: "resetGame")
+        UserDefaults.standard.set(1, forKey: "level")
+        UserDefaults.standard.set(2, forKey: "daysTil")  // change to 7 on launch
+        UserDefaults.standard.set(0, forKey: "daysMissed")
+    }
 }
