5b7d2e1c5634ad0aaf58d80ba958f7bf8f729909
Banana Viking <bananavikingcoder@gmail.com>
2018-07-01 12:50:12 -0500

    refactored MessageViewController to be adjustable

--- Dailies/View Controllers/MessageViewController.swift
+++ Dailies/View Controllers/MessageViewController.swift
@@ -9,23 +9,25 @@
 import UIKit
 
 class MessageViewController: UIViewController {
+    var launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
     
     @IBAction func dismissMessageButton(_ sender: UIButton) {
-        self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
+        view.window?.rootViewController?.dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
-        if let imageView = self.view.viewWithTag(10) as? UIImageView {
-            imageView.layer.cornerRadius = 8
+        
+        guard let imageView = view.viewWithTag(10) as? UIImageView else { return }
+        imageView.layer.cornerRadius = 8
+        
+        guard let textView = view.viewWithTag(11) as? UITextView else { return }
+        
+        guard let dismissMessageButton = view.viewWithTag(12) as? UIButton else { return }
+        dismissMessageButton.layer.cornerRadius = 8
+        
+        if launchedBefore == false {
             imageView.image = UIImage(named: "advisorHappy")
-        }
-        
-        if let textView = self.view.viewWithTag(11) as? UITextView {
             textView.text = "Welcome to Habit Quest! My name is Maya, and I will be your advisor on your journey. Add some Dailies when you are ready to begin the Skeleton Quest. You have a much better chance of surviving if you start small and build on wins."
-        }
-        
-        if let dismissMessageButton = self.view.viewWithTag(12) as? UIButton {
-            dismissMessageButton.layer.cornerRadius = 8
             dismissMessageButton.setTitle("Start Game", for: .normal)
         }
     }
