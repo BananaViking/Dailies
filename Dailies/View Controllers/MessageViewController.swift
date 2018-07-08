4dd58b4671a5ca870a16baafd254040f1928abb4
Banana Viking <bananavikingcoder@gmail.com>
2018-07-08 14:01:26 -0500

    change MessageVC textView to a label

--- Dailies/View Controllers/MessageViewController.swift
+++ Dailies/View Controllers/MessageViewController.swift
@@ -30,51 +30,51 @@ class MessageViewController: UIViewController {
         guard let imageView = view.viewWithTag(10) as? UIImageView else { return }
         imageView.layer.cornerRadius = 8
         
-        guard let textView = view.viewWithTag(11) as? UITextView else { return }
+        guard let messageLabel = view.viewWithTag(11) as? UILabel else { return }
         
         guard let dismissMessageButton = view.viewWithTag(12) as? UIButton else { return }
         dismissMessageButton.layer.cornerRadius = 8
         
         if launchedBefore == false {  // check to make sure you need ALL the UD resets even if you need SOME
             imageView.image = UIImage(named: "advisorHappy")
-            textView.text = "Welcome to Habit Quest! My name is Maya, and I will be your advisor on your journey. Add some Dailies when you are ready to begin the Skeleton Quest. You have a much better chance of surviving if you start small and build on wins."
+            messageLabel.text = "Welcome to Habit Quest! My name is Maya, and I will be your advisor on your journey. Add some Dailies when you are ready to begin the Skeleton Quest. You have a much better chance of surviving if you start small and build on wins."
             dismissMessageButton.setTitle("Start Game", for: .normal)
             dailiesVC.playSound(forObject: "firstLaunch")
         } else if beatGame == true {
             imageView.image = UIImage(named: "beatGame")
-            textView.text = "You have defeated the necromancer and saved the world. Go have a beer. You earned it."
+            messageLabel.text = "You have defeated the necromancer and saved the world. Go have a beer. You earned it."
             dismissMessageButton.setTitle("DRINK BEER", for: .normal)
             dailiesVC.playSound(forObject: "beatGame")
         } else if lostGame == true {
             imageView.image = UIImage(named: "lostGame")
-            textView.text = "You have been killed by the necromancer and his minions. Now the world will surely plunge into eternal darkness."
+            messageLabel.text = "You have been killed by the necromancer and his minions. Now the world will surely plunge into eternal darkness."
             dismissMessageButton.setTitle("TRY NOT TO FUCK IT UP THIS TIME", for: .normal)
             dailiesVC.playSound(forObject: "lostGame")
         } else if noDailies == true {
             imageView.image = UIImage(named: "advisorMad")
-            textView.text = "You must add at least one Daily before returning to your quest. Hurry up before it's too late!"
+            messageLabel.text = "You must add at least one Daily before returning to your quest. Hurry up before it's too late!"
             dismissMessageButton.setTitle("OK", for: .normal)
             dailiesVC.playSound(forObject: "noDailies")
         } else if gainLevel == true {
             imageView.image = UIImage(named: "advisorHappy")
-            textView.text = "Victory! You have vanquished the enemy and gained a level! There is no time to rest, however, as your next quest has already begun!"
+            messageLabel.text = "Victory! You have vanquished the enemy and gained a level! There is no time to rest, however, as your next quest has already begun!"
             dismissMessageButton.setTitle("OK", for: .normal)
             dailiesVC.playSound(forObject: "gainLevel")
             UserDefaults.standard.set(false, forKey: "gainLevel")
         } else if completeDailies == true {
             imageView.image = UIImage(named: "advisorHappy")
-            textView.text = "Well done! Yesterday you completed all of your Dailies. Keep it up and you will actually complete this quest with your head intact!"
+            messageLabel.text = "Well done! Yesterday you completed all of your Dailies. Keep it up and you will actually complete this quest with your head intact!"
             dismissMessageButton.setTitle("OK", for: .normal)
             dailiesVC.playSound(forObject: "completeDailies")
         } else if loseLevel == true {
             imageView.image = UIImage(named: "advisorMad")
-            textView.text = "You have been defeated and lost a level. If you can't keep up, perhaps you should set a reminder, drop a Daily, or make it easier."
+            messageLabel.text = "You have been defeated and lost a level. If you can't keep up, perhaps you should set a reminder, drop a Daily, or make it easier."
             dismissMessageButton.setTitle("OK", for: .normal)
             dailiesVC.playSound(forObject: "loseLevel")
             UserDefaults.standard.set(false, forKey: "loseLevel")
         } else if missDailies == true {
             imageView.image = UIImage(named: "advisorMad")
-            textView.text = "You did not complete your dailies yesterday. You must do better today or you will surely be defeated."
+            messageLabel.text = "You did not complete your dailies yesterday. You must do better today or you will surely be defeated."
             dismissMessageButton.setTitle("OK", for: .normal)
             dailiesVC.playSound(forObject: "missDailies")
         }
